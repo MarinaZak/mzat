8ca61443b942223be1c70f1933abb94925f93277
MarinaZak <marinna27.01.2002@gmail.com>
2022-06-12 18:23:28 +0300

    .

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -1,20 +1,20 @@
-create schema '3AT_21';
+use mzat;
 
-create table '3AT_21'.'student' (
-                                    'id_student' INT NOT NULL,
-                                    'name' VARCHAR(45) NULL,
-                                    'bd' DATE NULL,
-                                    'course' INT NULL,
-                                    PRIMARY KEY ('id_student'));
+create table student (
+                                    id_student INT NOT NULL,
+                                    name VARCHAR(45) NULL,
+                                    bd DATE NULL,
+                                    course INT NULL,
+                                    PRIMARY KEY (id_student));
 
-create table '3AT_21'.'address' (
-                                    'id_address' INT NOT NULL,
-                                    'country' VARCHAR(45) NULL,
-                                    'city' VARCHAR(45) NULL,
-                                    PRIMARY KEY ('id_address'));
+create table address (
+                                    id_address INT NOT NULL,
+                                    country VARCHAR(45) NULL,
+                                    city VARCHAR(45) NULL,
+                                    PRIMARY KEY (id_address));
 
-create table '3AT_21'.'student_address' (
-                                    'id_address_student' INT NOT NULL,
-                                    'id_address' VARCHAR(45) NULL,
-                                    'id_student' VARCHAR(45) NULL,
-                                    PRIMARY KEY ('id_address_student'));
+create table student_address (
+                                    id_address_student INT NOT NULL,
+                                    id_address VARCHAR(45) NULL,
+                                    id_student VARCHAR(45) NULL,
+                                    PRIMARY KEY (id_address_student));
